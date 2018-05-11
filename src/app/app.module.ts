f6b091d582145e92ab06e6ed353f7ad01b576f97
Irtiqa Ahmed <irtiqa@sunland.ca>
2018-05-10 20:06:52 -0400

    added form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home.component';
@@ -17,7 +18,8 @@ import { FormComponent } from './form/form.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styles: []
+  styles: [`.form-control.ng-invalid{ border: 1px solid red; }`]
 })
 export class FormComponent implements OnInit {
+  contactForm: FormGroup;
+  firstName: string;
+  lastName: string;
+  emailAddress: string;
+  comments: string;
+  maxAllowedCharacter: number = 50;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      fName: ['', Validators.required],
+      lName: ['', Validators.compose([Validators.required])],
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      comment: ['', Validators.compose([Validators.required, Validators.maxLength(this.maxAllowedCharacter)])]
+    })
+  }
 
   ngOnInit() {
+  }
+
+  onSubmit(data) {
+    console.log(data)
+    this.firstName = data.fname;
+    this.lastName = data.lName;
+    this.emailAddress = data.email;
+    this.comments = data.comment;
+  }
+  resetForm() {
+    this.firstName = null;
+    this.lastName = null;
+    this.emailAddress = null;
+    this.comments = null;
   }
 
 }
